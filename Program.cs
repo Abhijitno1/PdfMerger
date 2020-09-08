7ec16706dca116fc8841fd5bd45d5f1063fe2222
Abhijit Desai <abhijitno1@yahoo.com>
2020-09-07 22:09:55 -0400

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace PdfMerger
+{
+    class Program
+    {
+        static string inputFolder;
+        static string inputFilePrefix;
+        static string maxInputFileNumberTxt;
+        static string outputFilePathNname;
+
+        static void Main(string[] args)
+        {
+            inputFolder = ConfigurationManager.AppSettings["inputFolderName"];
+            inputFilePrefix = ConfigurationManager.AppSettings["inputFilePrefix"];
+            maxInputFileNumberTxt = ConfigurationManager.AppSettings["maxInputFileNumber"];
+            outputFilePathNname = ConfigurationManager.AppSettings["outputFilePathNname"];
+            int maxInputFileNumber = 1;
+            int.TryParse(maxInputFileNumberTxt, out maxInputFileNumber);
+
+            TraverseInputFolder();
+        }
+
+        static void TraverseInputFolder()
+        {
+            //if (!Directory.Exists(outputFolder)) Directory.CreateDirectory(outputFolder);
+            DirectoryInfo dirInfo = new DirectoryInfo(inputFolder);
+            var inputFileNames = dirInfo.GetFiles().Select(fileInfo => fileInfo.FullName).ToArray();
+            const char separator = '_';
+            string inputFilePrefix = null;
+            var fileNumbers = inputFileNames.Select(fileName =>
+            {
+                var sepPos = fileName.LastIndexOf(separator);
+                if (sepPos > -1)
+                {
+                    inputFilePrefix = fileName.Substring(0, sepPos);
+                    var extnPos = fileName.LastIndexOf('.');
+                    return int.Parse(fileName.Substring(sepPos + 1, extnPos - sepPos - 1));
+                }
+                else
+                    return -1;
+            });
+            Array.Sort(fileNumbers.Where(num => num != -1).ToArray());
+            inputFileNames = fileNumbers.Select(num => inputFilePrefix + separator + num + ".pdf").ToArray();
+            if (inputFileNames.Length > 1)
+                SimplePdfOps.CombineMultiplePDFs(inputFileNames, outputFilePathNname);
+        }
+    }
+}
---
+++ SimplePdfOps.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+
+namespace PdfMerger
+{
+    public class SimplePdfOps
+    {
+        //Reflink: https://stackoverflow.com/questions/6029142/merging-multiple-pdfs-using-itextsharp-in-c-net
+        public static void CombineMultiplePDFs(string[] fileNames, string outFile)
+        {
+            // step 1: creation of a document-object
+            Document document = new Document();
+            //create newFileStream object which will be disposed at the end
+            using (FileStream newFileStream = new FileStream(outFile, FileMode.Create))
+            {
+                // step 2: we create a writer that listens to the document
+                PdfCopy writer = new PdfCopy(document, newFileStream);
+                if (writer == null)
+                {
+                    return;
+                }
+
+                // step 3: we open the document
+                document.Open();
+
+                foreach (string fileName in fileNames)
+                {
+                    // we create a reader for a certain document
+                    PdfReader reader = new PdfReader(fileName);
+                    reader.ConsolidateNamedDestinations();
+
+                    // step 4: we add content
+                    for (int i = 1; i <= reader.NumberOfPages; i++)
+                    {
+                        PdfImportedPage page = writer.GetImportedPage(reader, i);
+                        writer.AddPage(page);
+                    }
+
+                    /*PRAcroForm form = reader.AcroForm;
+                    if (form != null)
+                    {
+                        writer.CopyAcroForm(reader);
+                    }*/
+
+                    reader.Close();
+                }
+
+                // step 5: we close the document and writer
+                writer.Close();
+                document.Close();
+            }//disposes the newFileStream object
+        }
+        public static void MergeFiles(string destinationFile, string[] sourceFiles)
+        {
+
+            try
+            {
+                int f = 0;
+                // we create a reader for a certain document
+                PdfReader reader = new PdfReader(sourceFiles[f]);
+                // we retrieve the total number of pages
+                int n = reader.NumberOfPages;
+                //Console.WriteLine("There are " + n + " pages in the original file.");
+                // step 1: creation of a document-object
+                Document document = new Document(reader.GetPageSizeWithRotation(1));
+                // step 2: we create a writer that listens to the document
+                PdfWriter writer = PdfWriter.GetInstance(document, new FileStream(destinationFile, FileMode.Create));
+                // step 3: we open the document
+                document.Open();
+                PdfContentByte cb = writer.DirectContent;
+                PdfImportedPage page;
+                int rotation;
+                // step 4: we add content
+                while (f < sourceFiles.Length)
+                {
+                    int i = 0;
+                    while (i < n)
+                    {
+                        i++;
+                        document.SetPageSize(reader.GetPageSizeWithRotation(i));
+                        document.NewPage();
+                        page = writer.GetImportedPage(reader, i);
+                        rotation = reader.GetPageRotation(i);
+                        if (rotation == 90 || rotation == 270)
+                        {
+                            cb.AddTemplate(page, 0, -1f, 1f, 0, 0, reader.GetPageSizeWithRotation(i).Height);
+                        }
+                        else
+                        {
+                            cb.AddTemplate(page, 1f, 0, 0, 1f, 0, 0);
+                        }
+                        //Console.WriteLine("Processed page " + i);
+                    }
+                    f++;
+                    if (f < sourceFiles.Length)
+                    {
+                        reader = new PdfReader(sourceFiles[f]);
+                        // we retrieve the total number of pages
+                        n = reader.NumberOfPages;
+                        //Console.WriteLine("There are " + n + " pages in the original file.");
+                    }
+                }
+                // step 5: we close the document
+                document.Close();
+            }
+            catch (Exception e)
+            {
+                string strOb = e.Message;
+            }
+        }
+    }
+
+}
